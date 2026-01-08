e29d93a15091120f14e303c1efca2b94514b61a9
Kim, Hyeonseo <hsys20020702@gmail.com>
2026-01-08 13:13:31 +0900

    swagger 수정

--- src/main/java/com/studycafe/config/SecurityConfig.java
+++ src/main/java/com/studycafe/config/SecurityConfig.java
@@ -59,7 +59,9 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth ->
                         auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
